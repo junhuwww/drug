cc5f6769b37dfd1a10447b57be03d4fae197488e
hujun <hujun@hydee.cn>
2020-11-06 11:09:35 +0800

    Initial commit

---
+++ src/main/java/cn/hydee/ydjia/drug/YdjiaDrugApplication.java
@@ -0,0 +1,13 @@
+package cn.hydee.ydjia.drug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YdjiaDrugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YdjiaDrugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/hydee/ydjia/drug/YdjiaDrugApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hydee.ydjia.drug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YdjiaDrugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
